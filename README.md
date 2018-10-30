a03e91804d5c7b08afacff462dbef859142f706b
Eyevolution <aiguan@ilooktech.com>
2018-10-30 15:41:13 +0900

    add LeadSense examples

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
+Original readme of YGZ-stereo-inertial SLAM is saved in README_origin.md. This project is forked from YGZ-stereo-inertial-SLAM. We add LeadSense examples.
+
 # YGZ-Stereo-Inertial
 This is YGZ-stereo-inertial SLAM, a stereo inertial VO code. It is designed for stereo and stereo-inertial sensor modules like vi-sensor. It uses a LK optical flow as front-end and a sliding window bundle adjustment as a backend. Feel free to try it in datasets and your own sensors. 
 
-The code is experimental and I can't guarantee  its performance in all cases.
+The code is experimental and I can't guarantee its performance in all cases.
 
 # Dependency
-If you are using ubuntu, just type "./install_dep.sh" to install all the dependencies except pangolin.
+If you are using ubuntu, just type "./install_dep.sh" to install all the dependencies except EvoBinoSDK and pangolin.
 
+- EvoBinoSDK:
+	- Download the latest version of the EvoBinoSDK on [LeadSense official site](http://leadsense.ilooktech.com/developer).
+	- For more EvoBinoSDK information, read the [Technical Documents](http://leadsense.ilooktech.com/sdk/docs/).
 - Pangolin (for visualization): https://github.com/stevenlovegrove/Pangolin 
 - Eigen3: sudo apt-get install libeigen3-dev
 - g2o: sudo apt-get install libcxsparse-dev libqt4-dev libcholmod3.0.6 libsuitesparse-dev qt4-qmake 
@@ -29,6 +34,47 @@ bin/EurocStereoVIO ./examples/EurocStereoVIO.yaml
 ```
 
 to run the stereo VIO case.
+
+# LeadSense examples
+
+There are two LeadSense examples: Stereo / Stereo + IMU. Examples can run with camera real-time or with an .evo file offline. Yaml file will be generated automatically.
+
+- Stereo example (640 * 400), run:
+
+```
+bin/leadsense
+```
+
+If you wish to run with a higher resolution (1280 * 800), run:
+
+```
+bin/leadsense 800
+
+```
+
+If you wish to run with an .evo file (eg. xxx.evo), run:
+
+```
+bin/leadsense xxx.evo
+```
+
+- Stereo + IMU example (640 * 400), run:
+
+```
+bin/leadsense_imu
+```
+
+If you wish to run with a higher resolution (1280 * 800), run:
+
+```
+bin/leadsense_imu 800
+```
+
+If you wish to run with an .evo file (eg. xxx.evo), run:
+
+```
+bin/leadsense_imu xxx.evo
+```
 
 # Other things
 YGZ-stereo is more robust than the previous YGZ-ORB-SLAM (and the code is also more clear). In EUROC it can pass the test of all MHxx and V101, V201, V202. For difficult cases it may still fail. As I will go to TUM soon, my future work may be a visual-inertial DSO.
---
+++ leadsense/leadsense.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+
+#include "opencv2/opencv.hpp"
+
+//YGZ header
+#include "ygz/System.h"
+
+//Evo SDK header
+#include "evo_stereocamera.h"//stereo camera
+#include "evo_matconverter.h"//evo Mat converter
+
+
+bool genYaml(evo::bino::StereoParameters param, evo::bino::Resolution_FPS resfps) {
+	ofstream fs("leadsense.yaml", ios_base::out);
+	if (!fs.is_open()) {
+		std::cerr << "error to open leadsense.yaml" << std::endl;
+		return false;
+	}
+	fs << "%YAML:1.0" << std::endl << "---" << std::endl << std::endl;
+	
+	fs << "PureVisionMode: true" << std::endl << std::endl;
+	
+	fs << "UseViewer: true" << std::endl << std::endl;
+
+	fs << "Camera.fx: " << param.leftCam.focal.x << std::endl;
+	fs << "Camera.fy: " << param.leftCam.focal.y << std::endl;
+	fs << "Camera.cx: " << param.leftCam.center.x << std::endl;
+	fs << "Camera.cy: " << param.leftCam.center.y << std::endl;
+	fs << "Camera.k1: " << 0.0 << std::endl;
+	fs << "Camera.k2: " << 0.0 << std::endl;
+	fs << "Camera.p1: " << 0.0 << std::endl;
+	fs << "Camera.p2: " << 0.0 << std::endl;
+	fs << "Camera.width: " << resfps.width << std::endl;
+	fs << "Camera.height: " << resfps.height << std::endl;
+	// Camera frames per second 
+	fs << "Camera.fps: " << resfps.fps << std::endl;
+	// stereo baseline[m] times fx
+	fs << "Camera.bf: " << param.baseline() / 1000 * param.leftCam.focal.x << std::endl;
+
+	fs.close();
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+    google::InitGoogleLogging(argv[0]);
+
+	std::cout << std::endl << "Usage: ./leadsense [400(default)|800|.evo file]" << std::endl;
+	
+	evo::bino::StereoCamera camera;
+	evo::bino::GrabParameters grab_parameters;
+	grab_parameters.do_rectify = true;
+	grab_parameters.calc_disparity = false;
+	int width, height;
+
+	int cnt = 0, limit = 100;
+	double tempt = 0;
+	
+	evo::RESULT_CODE res;
+
+	//open camera	
+	if (argc > 1)
+	{
+		int res_n = atoi(argv[1]);
+		if (res_n == 400)
+		{
+			res = camera.open(evo::bino::RESOLUTION_FPS_MODE_SD400_30);
+		}
+		else if (res_n == 800)
+		{	
+			res = camera.open(evo::bino::RESOLUTION_FPS_MODE_HD800_30);
+		}
+		else
+		{		
+			res = camera.open(argv[1]);
+		}
+	}
+	else
+	{
+		res = camera.open(evo::bino::RESOLUTION_FPS_MODE_SD400_30);
+	}
+	std::cout << "camera open: " << evo::result_code2str(res) << std::endl;
+
+	if (res == evo::RESULT_CODE_OK)//open camera successed
+	{
+		//show image size
+		width = camera.getImageSizeFPS().width;
+		height = camera.getImageSizeFPS().height;
+		std::cout << "image width:" << width << ", height:" << height << std::endl;
+
+		if (genYaml(camera.getStereoParameters(true), camera.getImageSizeFPS()) == false) {
+			return 2;
+		}
+
+		// Create SLAM system. It initializes all system threads and gets ready to process frames.
+		ygz::System system("leadsense.yaml");
+
+		std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+		//running flag
+		bool running = true;
+		//main loop
+		while (running)
+		{
+			// Get frames and launch the computation
+			res = camera.grab(grab_parameters);
+			if (res == evo::RESULT_CODE_OK)
+			{
+				evo::Mat<unsigned char> evo_left = camera.retrieveImage(evo::bino::SIDE_LEFT);
+				evo::Mat<unsigned char> evo_right = camera.retrieveImage(evo::bino::SIDE_RIGHT);
+				
+				//Mat convert
+				cv::Mat imLeft = evoMat2cvMat(evo_left);
+				cv::Mat imRight = evoMat2cvMat(evo_right);
+
+				double tframe = camera.getCurrentFrameTimeCode();
+
+				// Pass the images to the SLAM system
+		        system.AddStereo(imLeft, imRight, tframe);
+
+				double ttrack = std::chrono::duration_cast<std::chrono::duration<double> >(std::chrono::steady_clock::now() - t1).count();
+				t1 = std::chrono::steady_clock::now();
+
+				cnt++;
+				tempt += ttrack;
+				if (cnt == limit) {
+					cout << "avg loop time:" << tempt / limit << endl;
+					cnt = 0;
+					tempt = 0;
+				}
+			}
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+		camera.close();
+		// Stop all threads
+		system.Shutdown();
+	}
+
+	return 0;
+}
+
